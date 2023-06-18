deb13babdc21a2c9b9e7c4d6fb7f39681dbe2d34
cordfox <130101739+cordfox@users.noreply.github.com>
2023-06-18 18:39:40 -0500

    Update download.ps1
    This is from the github app.

--- download.ps1
+++ download.ps1
@@ -1,4 +1,4 @@
-#updated comments again from cordfox-development
+#updated comments again from cordfox-development. Maybe. 
 
 $sourceFiles = "https://github.com/cordfox/servertools/archive/refs/heads/main.zip"
 $downloadName = "servertools.zip"
