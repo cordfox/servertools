2fe08b387d191b98c096d186759941b278487026
cordfox <130101739+cordfox@users.noreply.github.com>
2023-06-18 09:16:14 -0500

    Update checkwindows-evalLicense.ps1

--- winlicense/checkwindows-evalLicense.ps1
+++ winlicense/checkwindows-evalLicense.ps1
@@ -1,2 +1,9 @@
-$timeRemaining = cscript $Env:SystemRoot\system32\slmgr.vbs /dlv | Select-String '\W*(Remaining Windows rearm count:\W*.*)'
-$timeRemaining = $timeRemaining -replace "[^0-9]"
+#Get Grace Period
+$GracePeriodRemaining = Get-CimInstance -ClassName SoftwareLicensingProduct | Select-Object -Expand GracePeriodRemaining
+
+#Do math to make things right
+$DaysRemaining = $GracePeriodRemaining/1440
+$Days = [math]::truncate($DaysRemaining)
+
+#This part sends the data back to Zabbix
+write-host $Days
