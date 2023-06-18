69f1b1e12262cb02ba27c535e2bde09cf714a048
cordfox <130101739+cordfox@users.noreply.github.com>
2023-06-18 17:16:06 -0500

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -1,7 +1,7 @@
-if(test-path C:\servertools-main){Remove-Item C:\servertools-main -Recurse -Force -Confirm:$FALSE };if(test-path C:\servertools.zip){Remove-Item C:\servertools.zip -Recurse -Force -Confirm:$FALSE}
-Invoke-WebRequest -Uri 'https://github.com/cordfox/servertools/archive/refs/heads/main.zip' -OutFile "C:\servertools.zip" 
-Expand-Archive C:\servertools.zip C:\
-Remove-Item C:\servertools.zip
-if(test-path C:\servertools){remove-item C:\servertools -Recurse -Force -Confirm:$FALSE}
-Rename-Item C:\servertools-main C:\servertools
-if(test-path C:\download.ps1){remove-item -Path C:\download.ps1}
+if(test-path C:\scripts\servertools-main){Remove-Item C:\scripts\servertools-main -Recurse -Force -Confirm:$FALSE };if(test-path C:\scripts\servertools.zip){Remove-Item C:\scripts\servertools.zip -Recurse -Force -Confirm:$FALSE}
+Invoke-WebRequest -Uri 'https://github.com/cordfox/servertools/archive/refs/heads/main.zip' -OutFile "C:\scripts\servertools.zip" 
+Expand-Archive C:\scripts\servertools.zip C:\scripts
+Remove-Item C:\scripts\servertools.zip
+if(test-path C:\scripts\servertools){remove-item C:\scripts\servertools -Recurse -Force -Confirm:$FALSE}
+Rename-Item C:\scripts\servertools-main C:\scripts\servertools
+if(test-path C:\scripts\download.ps1){remove-item -Path C:\scripts\download.ps1}
