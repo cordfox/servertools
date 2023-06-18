b9debffeb55cef71bff8f09bb5b80eefd74d5762
cordfox <130101739+cordfox@users.noreply.github.com>
2023-06-18 18:38:24 -0500

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -1,4 +1,4 @@
-#updated comments again!
+#updated comments again from cordfox-development
 
 $sourceFiles = "https://github.com/cordfox/servertools/archive/refs/heads/main.zip"
 $downloadName = "servertools.zip"
